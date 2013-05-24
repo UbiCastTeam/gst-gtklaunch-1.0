0bc548c630afcd43135b322ba9c3b177e0e12060
Dirk Van Haerenborgh <dirk.vanhaerenborgh@hogent.be>
2013-05-24 16:34:05 +0200

    fixed setup

--- setup.sh
+++ setup.sh
@@ -1,19 +1,6 @@
 #!/bin/sh
 PYTHON_VER=`python -V 2>&1 | sed "s/Python \([0-9][0-9]*\)\.\([0-9][0-9]*\)\.[0-9][0-9]*/\1.\2/"`
-PYTHON_24_DIR='python2.4/site-packages'
-PYTHON_25_DIR='python2.5/site-packages'
-PYTHON_26_DIR='python2.6/dist-packages'
-
-if [ "$PYTHON_VER"  = "2.4" ]
-then
-    PYTHON_DIR=$PYTHON_24_DIR
-elif [ "$PYTHON_VER"  = "2.5" ]
-then
-    PYTHON_DIR=$PYTHON_25_DIR
-elif [ "$PYTHON_VER"  = "2.6" ]
-then
-    PYTHON_DIR=$PYTHON_26_DIR
-fi
+PYTHON_DIR="python$PYTHON_VER/site-packages"
 
 PREFIX=/usr/local
 TARGET=$PREFIX/lib/$PYTHON_DIR/gstgengui
